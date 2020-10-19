611b410e46c71e8ab05255deba32d2b958159945
crazylimesucide <72753034+crazylimesucide@users.noreply.github.com>
2020-10-19 23:19:55 +0300

    1

---
+++ Лабораторна 3.1 If Else/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double x;
+	double y;
+	double A;
+	double B;
+	cout << "x = "; cin >> x;
+
+	A = (x*x/(2,1+sin(abs(x))));
+
+	if (x <= (-5))
+		(cos(exp(x))/sin(exp(x)));
+	else
+	if (-5 < x && x < 0)
+		(2-(pow(x,3)/(abs(x)+1)));
+	else
+		B = (log(sqrt(abs(x)-((x*x)/2))));
+	y = A + B;
+	cout << endl;
+	cout << "1) y = " << y << endl;	return 0;}
